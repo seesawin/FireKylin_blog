5d63ff8f6d6b80891dddabea2631c5118694fbe3
seesawin@gmail.com <seesaw1988>
2016-12-24 17:53:59 +0800

    2016/12/24 db info

---
+++ app/common/config/db.js
@@ -0,0 +1,19 @@
+
+      "use strict";
+      exports.__esModule = true;
+      exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "aa12scpv3uynwnc.cbwminojj9go.us-west-2.rds.amazonaws.com",
+            "port": "3306",
+            "database": "ebdb",
+            "user": "FFFF1234",
+            "password": "FFFF1234",
+            "prefix": "fk_",
+            "type": "mysql"
+        }
+    }
+}
+
+    
